574c5a39d64b2e8946e5c9119f3fbaffc5d043f2
JIPAN ALI <jipanali9593@gmail.com>
2024-12-12 22:36:58 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/hello', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! fro jip');
 });
 
 const PORT = process.env.PORT || 3000;
