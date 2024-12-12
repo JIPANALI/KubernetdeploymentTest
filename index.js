c4aa3b5d38ed7764c07f44276699e50a00aa02ce
JIPAN ALI <jipanali9593@gmail.com>
2024-12-13 01:22:51 +0530

    hi

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/hello', (req, res) => {
-  res.send('Hello, World! from jipss.');
+  res.send('Hello, World! from jipss. using kuber net');
 });
 
 const PORT = process.env.PORT || 3000;
