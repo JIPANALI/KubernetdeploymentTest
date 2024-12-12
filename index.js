3f973136067e30311461c197c27ffa2606abb62a
JIPAN ALI <jipanali9593@gmail.com>
2024-12-13 01:20:40 +0530

    Resolved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/hello', (req, res) => {
-  res.send('Hello, World! fro jipss');
+  res.send('Hello, World! from jipss.');
 });
 
 const PORT = process.env.PORT || 3000;
